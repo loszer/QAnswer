0c66684f1a322dba00e37053b3353a0b333bbfe6
loszer <loszer@163.com>
2016-10-20 06:16:48 +0800

    regular commit

--- main.py
+++
@@ -1,7 +0,0 @@
-from Tkinter import *
-
-root  =Tk()
-root.title('QAnswer')
-root.geometry('600x400')
-Button(root, text="choose file", font=('Arial', 20)).pack()
-root.mainloop()
---
+++ ui.py
@@ -0,0 +1,25 @@
+# -*- coding: UTF-8 -*-
+
+from Tkinter import *
+import tkFileDialog
+
+filename = ''
+
+def print_hello():
+    print 'hello world'
+
+def askfilename():
+    global filename
+    filename = tkFileDialog.askopenfilename()
+    print filename
+
+def main():
+    root = Tk()
+    root.title('QAnswer')
+    root.geometry('600x400')
+    Button(root,text='打开文件',command=askfilename).pack()
+    root.mainloop()
+    pass
+
+if __name__ == '__main__':
+    main()
